6cb882d2636e27fcf04389145e1c9db06419e212
Reclusious <93546348+Reclusious@users.noreply.github.com>
2022-07-24 10:04:12 -0500

    current progress

--- src/configuration/CalibrationConfig.h
+++ src/configuration/CalibrationConfig.h
@@ -32,7 +32,11 @@ namespace SlimeVR {
             // accelerometer offsets and correction matrix
             float A_B[3];
             float A_Ainv[3][3];
-
+            
+            // magnetometer offsets and correction matrix
+            float M_B[3];
+            float M_Ainv[3][3];
+            
             // raw offsets, determined from gyro at rest
             float G_off[3];
 
